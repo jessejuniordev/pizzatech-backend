d0abf3d5bba8746e2ed5aba0d985464fcc61ae0d
jessejuniordev <jessepcarvalhojunior02@gmail.com>
2025-03-12 18:22:39 -0300

    chore: setup project files

---
+++ src/test/java/io/github/jessejuniordev/pizzatech/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jessejuniordev.pizzatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
