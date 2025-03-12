7e31542294c6ebd4d373bfc8df3352394aee4425
jessejuniordev <jessepcarvalhojunior02@gmail.com>
2025-03-12 18:27:29 -0300

    feat: create TipoProduto model

---
+++ src/main/java/io/github/jessejuniordev/pizzatech/model/produtos/TipoProduto.java
@@ -0,0 +1,26 @@
+package io.github.jessejuniordev.pizzatech.model.produtos;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_tipo_produto")
+@Data
+public class TipoProduto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false)
+    private UUID id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 100, nullable = false)
+    private Produtos tipo;
+
+    @OneToMany(mappedBy = "tipoProduto")
+    private List<Produto> produtos;
+
+}
